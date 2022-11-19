f6f4b30490172e2f899b2836661b1021e823f94b
aallooss <44009215+aallooss@users.noreply.github.com>
2022-11-19 01:36:26 -0500

    change printing response

--- TCP_server.py
+++ TCP_server.py
@@ -10,13 +10,13 @@ class Handler_TCPServer(socketserver.BaseRequestHandler):
     def handle(self):
         # self.request - TCP socket connected to the client
         command = self.request.recv(1024).strip().decode('utf-8')
-        print(command)
         command_loaded = json.loads(command) #data loaded
 
         print(command_loaded)
         #print(battery.get_battery_subscription(battery_param))
         #message = str(battery.get_battery_subscription(battery_param))
         # just send back ACK for data arrival confirmation
+        message = "Response Received"
         self.request.sendall(message.encode())
 
 
@@ -24,7 +24,7 @@ class Handler_TCPServer(socketserver.BaseRequestHandler):
 
 #  IP and PORT configuration is to be set up here
 if __name__ == "__main__":
-    HOST, PORT = "172.20.10.5", 9999
+    HOST, PORT = "172.20.10.2", 8888
 
     # Init the TCP server object, bind it to the chosen HOST and PORT
     tcp_server = socketserver.TCPServer((HOST, PORT), Handler_TCPServer)
